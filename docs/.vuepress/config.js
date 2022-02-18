035c7f294132e77e41e48d2c7e3d429754d33005
Luckykeeper <luckykeeper@luckykeeper.site>
2022-02-18 22:58:53 +0800

    更新评论插件参数
    Co-Authored-By: LOVE69_Renpy_Remaster_Project <love69renpyremasterproject@luckykeeper.site>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -34,6 +34,7 @@ module.exports = {
             repo: 'Doc',
             clientId: '1cd4ed1eb3240fb3ac49',
             clientSecret: '00fdc3435f6a1bd301ba2b6986d3343452c45b3d',
+            autoCreateIssue: true,
           },
         // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
         repo: 'luckykeeper/LOVE69_renpy_remaster',
